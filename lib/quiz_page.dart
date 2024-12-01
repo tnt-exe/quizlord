ef2ab8995142f3d971c750dd4c9046749d527098
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-12-01 23:38:38 +0700

    use a list of maps for questions and implement checkAnswer function

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -10,13 +10,39 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    "You can lead a cow down stairs but not up stairs.",
-    "Approximately one quarter of human bones are in the feet.",
-    "A slug's blood is green.",
+  List<Map<String, bool>> questionsBank = [
+    {
+      'You can lead a cow down stairs but not up stairs.': false,
+    },
+    {
+      'Approximately one quarter of human bones are in the feet.': true,
+    },
+    {
+      "A slug's blood is green.": true,
+    },
   ];
 
   int questionProgress = 0;
+
+  void checkAnswer(int questionProgress, bool userPickedAnswer) {
+    bool correctAnswer = questionsBank[questionProgress].entries.first.value;
+
+    if (userPickedAnswer == correctAnswer) {
+      scoreKeeper.add(
+        const Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      );
+    } else {
+      scoreKeeper.add(
+        const Icon(
+          Icons.close,
+          color: Colors.red,
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,12 +53,12 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionProgress],
+                questionsBank[questionProgress].entries.first.key,
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -45,13 +71,8 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
               onPressed: () {
+                checkAnswer(questionProgress, true);
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
                   questionProgress++;
                 });
               },
@@ -76,13 +97,8 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
               onPressed: () {
+                checkAnswer(questionProgress, false);
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-                  );
                   questionProgress++;
                 });
               },
