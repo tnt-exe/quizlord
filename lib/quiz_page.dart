70953db8c8f4a8cf58810a45c96704077554ead7
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-12-01 23:26:36 +0700

    question sample and progress track

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -8,24 +8,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    "You can lead a cow down stairs but not up stairs.",
+    "Approximately one quarter of human bones are in the feet.",
+    "A slug's blood is green.",
   ];
+
+  int questionProgress = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -33,13 +24,13 @@ class _QuizPageState extends State<QuizPage> {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionProgress],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +52,7 @@ class _QuizPageState extends State<QuizPage> {
                       color: Colors.green,
                     ),
                   );
+                  questionProgress++;
                 });
               },
               style: TextButton.styleFrom(
@@ -83,7 +75,17 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
+                  );
+                  questionProgress++;
+                });
+              },
               style: TextButton.styleFrom(
                 backgroundColor: Colors.red,
                 shape: RoundedRectangleBorder(
