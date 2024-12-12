4f1bd3873c439b5d4dd816fd73b932ea9f988a23
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-12-12 17:41:49 +0700

    chore: add new question

--- lib/quiz_manage.dart
+++ lib/quiz_manage.dart
@@ -5,12 +5,30 @@ class QuizManage {
 
   final List<Quiz> _quizBank = [
     Quiz(
-        question: "You can lead a cow down stairs but not up stairs.",
+        question:
+            "Hypertext Transfer Protocol (HTTP) allows retrieving inter-linked text documents",
+        answer: true),
+    Quiz(
+        question:
+            "Uniform interface: all resources are accessed with a generic interface (e.g., HTTP GET, POST, PUT, DELETE)",
+        answer: true),
+    Quiz(
+        question:
+            "A RESTful Web service is like Web applications, but for human",
+        answer: false),
+    Quiz(question: "A <div> block is an inline-level element", answer: false),
+    Quiz(question: "BAML can't be decompiled back into XAML", answer: true),
+    Quiz(question: "Inline-level elements begin on a new line", answer: false),
+    Quiz(question: "True or False, Bootstrap is mobile-first", answer: true),
+    Quiz(
+        question:
+            "Unlike earlier versions of HTML, HTML5 produces pages that look the same across all browsers",
         answer: false),
     Quiz(
-        question: "Approximately one quarter of human bones are in the feet.",
+        question:
+            "HTML5 tags have the same semantic meaning, regardless of the browser being used",
         answer: true),
-    Quiz(question: "A slug's blood is green.", answer: true),
+    Quiz(question: "Block-level elements begin on a new line", answer: true),
   ];
 
   void nextQuestion() {
